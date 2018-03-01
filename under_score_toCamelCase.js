52ab70df029ee30376e263ed7dc9f95facc49208
aproios <25993548+aproios@users.noreply.github.com>
2018-03-01 11:10:52 -0800

    Update under_score_toCamelCase.js

--- under_score_toCamelCase.js
+++ under_score_toCamelCase.js
@@ -1,3 +1,8 @@
+/*under_score_toCamelCase is a simple Javascript script to convert any under_score string to camelCase.
+
+Ex: "Dont_cry_for_me_Argentina" converts to => "dontCryForMeArgentina" */
+
+
 function underToCamel(str){
   
   str = str.toLowerCase();
